48372f8862c81f9fd428de308bfc8356059ad317
tathagatag <50866778+tathagatag@users.noreply.github.com>
2019-07-19 17:15:19 +0530

    Update utcmng1_test.py

--- utcmng1_test.py
+++ utcmng1_test.py
@@ -1,13 +1,7 @@
-#import utcmng1
-#import pytest
-
-#def test_countrec():
-#        result = utcmng1.countrec()
-#        assert result == 18476
+import utcmng1
+import pytest
 
 def test_countrec():
-        print("Hello TG")
-        result = 18476
+       result = utcmng1.countrec()
         assert result == 18476
-
 test_countrec()
